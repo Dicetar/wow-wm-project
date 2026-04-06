6a615d6aac9323b3e410c21fe31460db94b5c403
Dicetar <88191847+Dicetar@users.noreply.github.com>
2026-04-07 02:09:19 +0300

    feat: add runtime sync package

---
+++ src/wm/runtime_sync/__init__.py
@@ -0,0 +1,13 @@
+from wm.runtime_sync.soap import (
+    RuntimeCommandResult,
+    RuntimeSyncResult,
+    SoapRuntimeClient,
+    build_default_quest_reload_commands,
+)
+
+__all__ = [
+    "RuntimeCommandResult",
+    "RuntimeSyncResult",
+    "SoapRuntimeClient",
+    "build_default_quest_reload_commands",
+]
