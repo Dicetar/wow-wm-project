e9a56504fad5a992c020e029f28bbef8f142f824
Dicetar <88191847+Dicetar@users.noreply.github.com>
2026-04-07 17:29:54 +0300

    feat: add spell slot pipeline package

---
+++ src/wm/spells/__init__.py
@@ -0,0 +1,7 @@
+from wm.spells.models import ManagedSpellDraft, ManagedSpellLink, ManagedSpellProcRule
+
+__all__ = [
+    "ManagedSpellDraft",
+    "ManagedSpellLink",
+    "ManagedSpellProcRule",
+]
